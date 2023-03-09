9afb91c08a9c0581bfeeb8d8effe54137e7db260
“Visesh-A” <viseshajl@gmail.com>
2023-03-09 23:34:52 +0530

    Add:ClassMethod

--- PartTimeWage/PartTimeWage.cs
+++ PartTimeWage/PartTimeWage.cs
@@ -3,7 +3,8 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
-        const int IS_PRESENT = 1, WAGE_PER_HR = 20, FULL_DAY_HR = 8, PART_TM_HR = 4, FULL_TIME = 0, PART_TIME = 1, TOTAL_WORKING_DAYS = 20; public void Attendance()
+        const int IS_PRESENT = 1, WAGE_PER_HR = 20, FULL_DAY_HR = 8, PART_TM_HR = 4, FULL_TIME = 0, PART_TIME = 1, TOTAL_WORKING_DAYS = 20, TOTAL_WORKING_HOURS = 100;
+        public void Attendance()
         {
             Random random = new Random();
             int check = random.Next(0, 2);
@@ -16,7 +17,7 @@ namespace EmployeeWageComputation
         {
             int empHrs = 0;
             Random random = new Random();
-            for (int i = 0; i <= TOTAL_WORKING_DAYS; i++)
+            for (int i = 0; i <= TOTAL_WORKING_DAYS && empHrs <= TOTAL_WORKING_HOURS; i++)
             {
                 int check = random.Next(0, 3);
                 switch (check)
@@ -24,13 +25,12 @@ namespace EmployeeWageComputation
                 {
                     case FULL_TIME:
                         empHrs += FULL_DAY_HR;
-                        empHrs = empHrs + FULL_DAY_HR;
                         break;
                     case PART_TIME:
                         empHrs += PART_TM_HR;
                         break;
                     default:
-                        empHrs = 0;
+                        empHrs += 0;
                         break;
                 }
             }
---
+++ PartTimeWage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.Attendance();
+            EmployeeWage employee = new EmployeeWage();
+            employee.DailyEmWages();
+        }
+    }
+}
